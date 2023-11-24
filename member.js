1ea1375f2a88eaf05465274477c55493c106ef6c
aigualada <aigualada@users.noreply.github.com>
2023-11-24 11:08:56 +0000

    Adding member.js

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember( {
+  var skills = ["HTML", "CSS", "JS", "React"];
+  var member = {
+    name: "John",
+    age: 23,
+    skills: skills,
+  };
+  // Write code here
+  var memberSkills = member.skills;
+  // End code
+  return memberSkills;
+});
